07a08da128db12af2660ecff0b6d5ba115571c25
Doug Guastaferro <dguastaferro@linkedin.com>
2017-02-12 22:24:36 -0800

    Image preprocessing done

--- image_processor.py
+++ image_processor.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 class ImageProcessor:
 
     def __init__(self, mtx, dist):
@@ -10,27 +11,96 @@ class ImageProcessor:
 
     def process_next_image(self, image, i):
         image = cv2.undistort(image, self.__mtx, self.__dist, None, self.__mtx)
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
-        gradx = self.__abs_sobel_thresh(image, orient='x', thresh_min=20, thresh_max=100)
-        grady = self.__abs_sobel_thresh(image, orient='y', thresh_min=50, thresh_max=200)
+        gradx = self.__abs_sobel_thresh(gray, orient='x', thresh=(40, 100))
+        grady = self.__abs_sobel_thresh(gray, orient='y', thresh=(50, 200))
 
-        combined = np.zeros_like(image)
+        combined = np.zeros_like(gray)
         combined[((gradx == 1) & (grady == 1))] = 1
 
-        plt.imsave('output_images/processed{}-x.jpg'.format(i), gradx , cmap='gray')
-        plt.imsave('output_images/processed{}-y.jpg'.format(i), grady , cmap='gray')
-        plt.imsave('output_images/processed{}-c.jpg'.format(i), combined , cmap='gray')
+        light = self.__hls_thresh(image, channel=1, thresh=(130, 255))
+        s = self.__hls_thresh(image, channel=2, thresh=(140, 255))
 
-    def __abs_sobel_thresh(self, img, orient='x', thresh_min=0, thresh_max=255):
-        
-        sobelx = np.absolute(cv2.Sobel(img, cv2.CV_64F, 1, 0))
-        sobely = np.absolute(cv2.Sobel(img, cv2.CV_64F, 0, 1))
+        ls = np.zeros_like(light)
+        ls[((light == 1) & (s == 1))] = 1
 
-        sobel = np.sqrt(np.square(sobelx) + np.square(sobely))
+        total = np.zeros_like(combined)
+        total[((combined == 1) | (ls == 1))] = 1
+
+        f, p = plt.subplots(3, 3)
+        # f, (ax1) = plt.subplots(1, 1)
+        f.tight_layout()
+
+        p[0][0].imshow(cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+        p[0][0].set_title('Original Image', fontsize=10)
+
+        p[0][1].imshow(gradx, cmap='gray')
+        p[0][1].set_title('Sobel X', fontsize=10)
+
+        p[0][2].imshow(grady, cmap='gray')
+        p[0][2].set_title('Sobel Y', fontsize=10)
+
+        p[1][0].imshow(combined, cmap='gray')
+        p[1][0].set_title('Combined Sobel X & Y', fontsize=10)
+
+        p[1][1].imshow(light, cmap='gray')
+        p[1][1].set_title('Lightness', fontsize=10)
+
+        p[1][2].imshow(s, cmap='gray')
+        p[1][2].set_title('Saturation', fontsize=10)
+
+        p[2][0].imshow(ls, cmap='gray')
+        p[2][0].set_title('Lightness & Saturation', fontsize=10)
+
+        p[2][1].imshow(total, cmap='gray')
+        p[2][1].set_title('Light & Sat OR Sobel X & Y', fontsize=10)
+
+        plt.show()
+
+    def __hls_thresh(self, img, channel=1, thresh=(0, 255)):
+        hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
+        single_channel = hsv[:, :, channel]
+        binary = np.zeros_like(single_channel)
+        binary[(single_channel >= thresh[0]) & (single_channel <= thresh[1])] = 1
+
+        return binary
+
+
+    def __abs_sobel_thresh(self, img, orient='x', kernel=3, thresh=(0, 255)):
+        sobel = None
+
+        if (orient == 'x'):
+            sobel = np.absolute(cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=kernel))
+        elif (orient == 'y'):
+            sobel = np.absolute(cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=kernel))
+        else:
+            print("Returning none")
+            return None
+
         scaled_sobel = np.uint8(255 * sobel / np.max(sobel))
 
         binary = np.zeros_like(scaled_sobel)
-        binary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+        binary[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
 
         return binary
+
+    def __mag_thresh(self, img, kernel=3, thresh=(0, 255)):
+        sobelx = cv2.Sobel(img, cv2.CV_64F, 0, 1)
+        sobely = cv2.Sobel(img, cv2.CV_64F, 1, 0)
+        abs_sobel = np.sqrt(np.square(sobelx) + np.square(sobely))
+        scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
+        sbinary = np.zeros_like(scaled_sobel)
+        sbinary[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+
+        return sbinary
+
+    def __dir_thresh(self, img, kernel=3, thresh=(0, np.pi / 2)):
+        sobelx = np.absolute(cv2.Sobel(img, cv2.CV_64F, 0, 1))
+        sobely = np.absolute(cv2.Sobel(img, cv2.CV_64F, 1, 0))
+
+        scaled_sobel = np.arctan2(sobely, sobelx)
+        sbinary = np.zeros_like(scaled_sobel)
+        sbinary[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+
+        return sbinary
--- lane_finder.py
+++ lane_finder.py
@@ -6,8 +6,8 @@ mtx, dist = calibrate_camera()
 
 processor = ImageProcessor(mtx, dist)
 
-image = cv2.imread('test_images/straight_lines1.jpg')
-processor.process_next_image(image, 0)
+# image = cv2.imread('test_images/straight_lines1.jpg')
+# processor.process_next_image(image, 0)
 
-image = cv2.imread('test_images/test1.jpg')
+image = cv2.imread('test_images/test6.jpg')
 processor.process_next_image(image, 1)
